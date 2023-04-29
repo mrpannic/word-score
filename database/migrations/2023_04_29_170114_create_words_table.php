af7b15f015d76a97fab2f196a983250cc920802a
Nikola Panic <nikola.panic@inkaviation.com>
2023-04-29 19:04:06 +0200

    word model and migration

---
+++ database/migrations/2023_04_29_170114_create_words_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('word');
+            $table->unsignedInteger('positive_count');
+            $table->unsignedInteger('negative_count');
+            $table->unsignedTinyInteger('source');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('words');
+    }
+};
